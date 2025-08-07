3185b59cfee964c78f1ed616ead0ff566c0d98e5
AliciaMiranda12 <mirandacaly@gmail.com>
2025-08-07 15:44:30 -0300

    Add files via upload

---
+++ Atividade DS/carro.js
@@ -0,0 +1,14 @@
+class Carro {
+    set portas(valor){
+        this._portas = valor;
+    }
+    get portas(){
+        return this._portas;
+    }
+    abrirPortas(){
+        return `A porta foi aberta`;
+
+    };
+
+
+};
---
+++ Atividade DS/main.js
@@ -0,0 +1,12 @@
+const v = new Veiculo();
+v.marca = "Toyota";
+v.ano = "2020";
+v.modelo = "Corolla";
+
+console.log(v.informacoes());
+console.log(v.ligar());
+
+const c = new Carro();
+c.portas = "4";
+
+console.log(c.abrirPortas());
